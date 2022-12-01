beadded8491b2c80fb4e1cacc3ce5e60d54f2bda
Victor Alberto <victoralbertomhp@gmail.com>
2022-12-01 03:25:18 -0600

    Add pollo-solutions-py

---
+++ pollo-solutions-py/templates/template.py
@@ -0,0 +1,20 @@
+import pathlib
+
+def get_input():
+    # Read input from pathlib
+    input = pathlib.Path('input.txt').read_text()...
+    return input
+
+    
+def part1(input):
+    pass
+
+
+
+def part2(input):
+    pass
+
+
+input = get_input()
+print(f"Part 1: {part1(input)}")
+print(f"Part 2: {part2(input)}")
