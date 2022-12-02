e832e313cf6f5d31d32b707a866e36173d8a8837
Anton Österberg <anton.osterberg@protonmail.com>
2022-12-02 07:06:06 +0100

    Day 2, part 2 completed

--- mantono-rust/src/day2.rs
+++ mantono-rust/src/day2.rs
@@ -5,7 +5,7 @@ use aoc::Solver;
 pub struct Aoc;
 
 impl Solver for Aoc {
-    type Item = Round;
+    type Item = Strategy;
 
     fn solve_one(inputs: &[Round]) -> String {
         inputs
@@ -15,10 +15,51 @@ impl Solver for Aoc {
             .sum::<usize>()
             .to_string()
     }
+
+    fn solve_two(inputs: &[Self::Item]) -> String {
+        inputs
+            .iter()
+            .map(|strat| (strat.play(), strat.1))
+            .map(|(shape, outome)| score(shape, outome))
+            .sum::<usize>()
+            .to_string()
+    }
 }
 
 fn score(shape: Shape, outcome: Outcome) -> usize {
     shape.score() + outcome.score()
+}
+
+#[derive(Copy, Clone)]
+pub struct Strategy(Shape, Outcome);
+
+impl Strategy {
+    fn play(&self) -> Shape {
+        match (self.0, self.1) {
+            (Shape::Rock, Outcome::Lost) => Shape::Scissors,
+            (Shape::Rock, Outcome::Draw) => Shape::Rock,
+            (Shape::Rock, Outcome::Won) => Shape::Paper,
+            (Shape::Paper, Outcome::Lost) => Shape::Rock,
+            (Shape::Paper, Outcome::Draw) => Shape::Paper,
+            (Shape::Paper, Outcome::Won) => Shape::Scissors,
+            (Shape::Scissors, Outcome::Lost) => Shape::Paper,
+            (Shape::Scissors, Outcome::Draw) => Shape::Scissors,
+            (Shape::Scissors, Outcome::Won) => Shape::Rock,
+        }
+    }
+}
+
+impl FromStr for Strategy {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = s.split_ascii_whitespace();
+        let shape = tokens.next().expect("Could not find shape");
+        let outcome = tokens.next().expect("Could not find outcome");
+        let shape: Shape = shape.parse()?;
+        let outcome: Outcome = outcome.parse()?;
+        Ok(Strategy(shape, outcome))
+    }
 }
 
 pub struct Round(Shape, Shape);
@@ -52,6 +93,7 @@ impl FromStr for Round {
     }
 }
 
+#[derive(Clone, Copy)]
 enum Outcome {
     Lost,
     Draw,
@@ -64,6 +106,19 @@ impl Outcome {
             Outcome::Lost => 0,
             Outcome::Draw => 3,
             Outcome::Won => 6,
+        }
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Lost),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Won),
+            _ => Err(format!("Invalid outcome {}", s)),
         }
     }
 }
