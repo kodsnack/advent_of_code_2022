f63f8f79666f1649bd7fd45fce24542c2898df05
Anton Österberg <anton.osterberg@protonmail.com>
2022-12-04 17:51:32 +0100

    Day 4, only use sets and no ranges

--- mantono-rust/src/day4.rs
+++ mantono-rust/src/day4.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::HashSet,
-    ops::{Range, RangeBounds},
-    str::FromStr,
-};
+use std::{collections::BTreeSet, str::FromStr};
 
 use aoc::Solver;
 
@@ -20,32 +16,20 @@ impl Solver for Aoc {
     }
 
     fn solve_two(inputs: &[Self::Item]) -> String {
-        inputs
-            .iter()
-            //.filter(|x| x.fully_contains())
-            .filter(|x| x.intersection().len() > 0)
-            //.reduce(|acc, e| acc.union(&e).copied().collect())
-            //.unwrap()
-            //.len()
-            .count()
-            .to_string()
+        inputs.iter().filter(|x| !x.intersection().is_empty()).count().to_string()
     }
 }
 
 #[derive(Debug)]
-pub struct Assignment(Range<u8>, Range<u8>);
+pub struct Assignment(BTreeSet<u8>, BTreeSet<u8>);
 
 impl Assignment {
     fn fully_contains(&self) -> bool {
-        let s0: HashSet<u8> = self.0.clone().into_iter().collect();
-        let s1: HashSet<u8> = self.1.clone().into_iter().collect();
-        s0.is_subset(&s1) || s1.is_subset(&s0)
+        self.0.is_subset(&self.1) || self.1.is_subset(&self.0)
     }
 
-    fn intersection(&self) -> HashSet<u8> {
-        let s0: HashSet<u8> = self.0.clone().into_iter().collect();
-        let s1: HashSet<u8> = self.1.clone().into_iter().collect();
-        s0.intersection(&s1).into_iter().copied().collect()
+    fn intersection(&self) -> BTreeSet<u8> {
+        self.0.intersection(&self.1).into_iter().copied().collect()
     }
 }
 
@@ -53,21 +37,18 @@ impl FromStr for Assignment {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.split(",");
+        let mut parts = s.split(',');
         let first = parts.next().expect("Invalid input");
         let second = parts.next().expect("Invalid input");
         Ok(Assignment(parse_range(first).unwrap(), parse_range(second).unwrap()))
     }
 }
 
-fn parse_range(input: &str) -> Result<Range<u8>, String> {
-    let mut parts = input.split("-");
+fn parse_range(input: &str) -> Result<BTreeSet<u8>, String> {
+    let mut parts = input.split('-');
     let start: u8 = parse_num(parts.next())?;
     let end: u8 = parse_num(parts.next())?;
-    Ok(Range {
-        start,
-        end: end + 1,
-    })
+    Ok(BTreeSet::from_iter(start..=end))
 }
 
 fn parse_num(n: Option<&str>) -> Result<u8, String> {
