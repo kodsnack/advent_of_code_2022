f4229f44cc28c1028554594bcf7a38e06fe95905
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2022-12-05 06:13:00 +0100

    what works

---
+++ estomagordo-python/5b.py
@@ -0,0 +1,62 @@
+from collections import Counter, defaultdict, deque
+from functools import cache, reduce
+from heapq import heapify, heappop, heappush
+from itertools import combinations, permutations, product
+from helpers import chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives
+
+
+def solve(lines):
+    crates = []
+    moves = []
+    addingto = crates
+
+    for line in lines:
+        if not line.rstrip():
+            addingto = moves
+        else:
+            addingto.append(line)
+
+    stacks = []
+
+    for line in crates:
+        n = len(line)
+
+        for x in range(1, n, 4):
+            if not line[x].isalpha():
+                continue
+
+            cratenum = x//4
+
+            while len(stacks) < cratenum+1:
+                stacks.append([])
+
+            stacks[cratenum] = [line[x]] + stacks[cratenum]
+
+    m = len(stacks)
+
+    for move in moves:
+        number, frm, to = ints(move)
+        
+        stacks[to-1].extend(stacks[frm-1][-number:])
+        stacks[frm-1] = stacks[frm-1][:-number]
+
+    s = ''
+
+    for stack in stacks:
+        s += stack[-1]
+
+    return s
+
+
+def main():
+    lines = []
+
+    with open('5.txt') as f:
+        for line in f.readlines():
+            lines.append(line)
+            
+    return solve(lines)
+
+
+if __name__ == '__main__':
+    print(main())
