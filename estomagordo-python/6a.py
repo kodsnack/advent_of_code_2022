1ea87ecd5fb9ff7d856465a8de5cdbb8036f8c49
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2022-12-06 06:10:52 +0100

    cleaned up

--- estomagordo-python/6a.py
+++ estomagordo-python/6a.py
@@ -5,23 +5,20 @@ from itertools import combinations, permutations, product
 from helpers import chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives
 
 
-def solve(lines):
-    x = 4
+def solve(signal):
+    length = 4
+    x = length
 
-    while len(set(lines[0][x-4:x])) < 4:
+    while len(set(signal[x-length:x])) < length:
         x += 1
 
     return x
 
 
 def main():
-    lines = []
-
     with open('6.txt') as f:
         for line in f.readlines():
-            lines.append(line)
-            
-    return solve(lines)
+            return solve(line)
 
 
 if __name__ == '__main__':
--- estomagordo-python/6b.py
+++ estomagordo-python/6b.py
@@ -5,23 +5,20 @@ from itertools import combinations, permutations, product
 from helpers import chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives
 
 
-def solve(lines):
-    x = 14
+def solve(signal):
+    length = 14
+    x = length
 
-    while len(set(lines[0][x-14:x])) < 14:
+    while len(set(signal[x-length:x])) < length:
         x += 1
 
     return x
 
 
 def main():
-    lines = []
-
     with open('6.txt') as f:
         for line in f.readlines():
-            lines.append(line)
-            
-    return solve(lines)
+            return solve(line)
 
 
 if __name__ == '__main__':
