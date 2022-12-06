5a4a9aa009dec476b5e8b21292f4943a88e32d31
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2022-12-06 06:04:22 +0100

    as it happened

---
+++ estomagordo-python/6a.py
@@ -0,0 +1,28 @@
+from collections import Counter, defaultdict, deque
+from functools import cache, reduce
+from heapq import heapify, heappop, heappush
+from itertools import combinations, permutations, product
+from helpers import chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives
+
+
+def solve(lines):
+    x = 4
+
+    while len(set(lines[0][x-4:x])) < 4:
+        x += 1
+
+    return x
+
+
+def main():
+    lines = []
+
+    with open('6.txt') as f:
+        for line in f.readlines():
+            lines.append(line)
+            
+    return solve(lines)
+
+
+if __name__ == '__main__':
+    print(main())
