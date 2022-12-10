702c7a14502002772f1fe4a0dc4cb7ddac0947d2
Gustav Westling <gustav@westling.dev>
2022-12-10 20:52:36 +0100

    d10 cleanup

--- zegl-go/d10/main.go
+++ zegl-go/d10/main.go
@@ -12,37 +12,30 @@ var input string
 
 func main() {
 	input = strings.TrimSpace(input)
+	rows := strings.Split(input, "\n")
 
 	var res int
 	var x int = 1
-
-	rows := strings.Split(input, "\n")
-
-	i := 0
-	ins := 0
+	var cycle, ins = 0, 0
 
 	step := func() {
-		i++
-		if i == 20 || i == 60 || i == 100 || i == 140 || i == 180 || i == 220 {
-			res += x * i
+		if (cycle-20)%40 == 0 {
+			res += x * cycle
 		}
-	}
 
-	pr := func() {
-		if i%40 == 0 {
-			fmt.Println("")
-		}
-		col := i % 40
-		if x == col || x == col-1 || x == col-2 {
+		col := ((cycle - 1) % 40)
+		if x == col || x == col-1 || x == col+1 {
 			fmt.Print("#")
 		} else {
 			fmt.Print(" ")
 		}
+		if col == 39 {
+			fmt.Println()
+		}
 	}
 
-	pr()
-
-	for i < 240 {
+	for cycle < 240 {
+		cycle++
 		row := rows[ins%len(rows)]
 		ins++
 
@@ -50,20 +43,14 @@ func main() {
 
 		if parts[0] == "addx" {
 			step()
-			pr()
-
+			cycle++
 			step()
-			pr()
-
 			v, _ := strconv.Atoi(parts[1])
 			x += v
 		} else if parts[0] == "noop" {
 			step()
-			pr()
 		}
 	}
 
-	fmt.Println()
-	fmt.Println()
 	fmt.Println(res)
 }
