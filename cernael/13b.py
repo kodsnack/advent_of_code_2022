9e9cd65f5ffcaaa0b0af58ea07daf6fd2f2aa377
Tomas Kronvall <tomas@cernael.com>
2022-12-14 09:52:03 +0100

    sands of time

--- cernael/13b.py
+++ cernael/13b.py
@@ -2,43 +2,46 @@ from functools import cmp_to_key
 
 def solve(lines):
     lines.extend([[[2]],[[6]]])
-    for l in lines: print(l)
-    [lines[0], lines[1]].sort(key=cmp_to_key(compare))
-    return
+    #for l in lines: print(l)
+    #lines = lines[-2:]
+    lines.sort(key=cmp_to_key(compare))
+    return (lines.index([[2]]) + 1) * (lines.index([[6]]) + 1)
 
 def compare(l,r):
+#    print(0,type(l),type(r))
     if isinstance(l, int) and isinstance(r, int):
+ #       print('ii', l,r)
         if l == r:
             return 0
         elif l < r:
             return -1
         return 1
-    if isinstance(l, int) and isinstance(r, list):
+    elif isinstance(l, int) and isinstance(r, list):
+  #      print('il', l,r)
         return compare([l], r)
-    if isinstance(l, list) and isinstance(r, int):
+    elif isinstance(l, list) and isinstance(r, int):
+   #     print('li', l,r)
         return compare(l, [r])
-    if isinstance(l, list) and isinstance(r, list):
+    elif isinstance(l, list) and isinstance(r, list):
+    #    print('ll', l,r)
         res = list(filter(lambda x:x != 0, map(lambda z: compare(z[0],z[1]),zip(l,r))))
         if len(res):
-            r = res[0]
-            if r == 1:
-                return 1
-            elif r == -1:
-                return -1
-            else:
-                return 0
+            return int(res[0])//abs(int(res[0]))
         if len(l) == len(r):
             return 0
         elif len(l) < len(r):
             return -1
         return 1
+    return 0
 
 
 
 if __name__ == '__main__':
     lines = []
-    with open('13test.txt') as f:
+    with open('13.txt') as f:
         for line in f.readlines():
             line = line.strip()
-            if line: lines.append(line)
+            if line:
+                line = eval(line)
+                lines.append(line)
     print(solve(lines))
