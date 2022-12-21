5f5376d207365cdb79131d625e882026f6d77753
Gustav Westling <gustav@westling.dev>
2022-12-21 08:48:34 +0100

    d21 (cleanup)

--- zegl-go/d21/main.go
+++ zegl-go/d21/main.go
@@ -28,63 +28,82 @@ func part1(words map[string]string) int {
 	var solve func(str string) int
 	solve = func(str string) int {
 		w := words[str]
-		if strings.Contains(w, "+") {
-			parts := strings.Split(w, " + ")
-			return solve(parts[0]) + solve(parts[1])
+		p := strings.Split(w, " ")
+		if len(p) == 1 {
+			r, _ := strconv.Atoi(w)
+			return r
 		}
-		if strings.Contains(w, "-") {
-			parts := strings.Split(w, " - ")
-			return solve(parts[0]) - solve(parts[1])
+		switch p[1] {
+		case "+":
+			return solve(p[0]) + solve(p[2])
+		case "-":
+			return solve(p[0]) - solve(p[2])
+		case "*":
+			return solve(p[0]) * solve(p[2])
+		case "/":
+			return solve(p[0]) / solve(p[2])
+		default:
+			panic("unknown operator")
 		}
-		if strings.Contains(w, "*") {
-			parts := strings.Split(w, " * ")
-			return solve(parts[0]) * solve(parts[1])
-		}
-		if strings.Contains(w, "/") {
-			parts := strings.Split(w, " / ")
-			return solve(parts[0]) / solve(parts[1])
-		}
-		r, _ := strconv.Atoi(w)
-		return r
 	}
 
 	return solve("root")
 }
 
 func part2(words map[string]string) int {
-
 	var solve func(str string, n int) int
 	solve = func(str string, n int) int {
 		if str == "humn" {
 			return n
 		}
 		w := words[str]
-		if strings.Contains(w, "+") {
-			parts := strings.Split(w, " + ")
-			return solve(parts[0], n) + solve(parts[1], n)
+		p := strings.Split(w, " ")
+		if len(p) == 1 {
+			r, _ := strconv.Atoi(w)
+			return r
 		}
-		if strings.Contains(w, "-") {
-			parts := strings.Split(w, " - ")
-			return solve(parts[0], n) - solve(parts[1], n)
+		switch p[1] {
+		case "+":
+			return solve(p[0], n) + solve(p[2], n)
+		case "-":
+			return solve(p[0], n) - solve(p[2], n)
+		case "*":
+			return solve(p[0], n) * solve(p[2], n)
+		case "/":
+			return solve(p[0], n) / solve(p[2], n)
+		default:
+			panic("unknown operator")
 		}
-		if strings.Contains(w, "*") {
-			parts := strings.Split(w, " * ")
-			return solve(parts[0], n) * solve(parts[1], n)
-		}
-		if strings.Contains(w, "/") {
-			parts := strings.Split(w, " / ")
-			return solve(parts[0], n) / solve(parts[1], n)
-		}
-		r, _ := strconv.Atoi(w)
-		return r
 	}
 
-	b := solve("tjtt", 0)
-	// lol manual bisection
-	for n := 3330805240000; ; n += 1 {
-		a := solve("bjgs", n)
+	min := 0
+	max := 10_000_000_000_000
+	var found int
+
+	lr := strings.Split(words["root"], " ")
+
+	for min < max {
+		mid := (min + max) / 2
+		a := solve(lr[0], mid)
+		b := solve(lr[2], mid)
 		if a == b {
-			return n
+			found = mid
+			break
+		}
+		if a > b {
+			min = mid + 1
+		} else {
+			max = mid
 		}
 	}
+
+	for i := found - 10000; i <= found; i++ {
+		a := solve(lr[0], i)
+		b := solve(lr[2], i)
+		if a == b {
+			return i
+		}
+	}
+
+	return -1
 }
